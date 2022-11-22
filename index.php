3ef463a629fb0bc4e2ad92f5aa00d428f2b6cdef
alexotalvaro <alejandromilles@outlook.com>
2022-11-22 13:41:57 +0100

    22/11/2022 master 2 commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
     </head>
     <body>
        <?php 
-        print 'Bienvenido a GIT 2';
+        print 'Bienvenido amster 3';
        ?>
     </body>
 </html>
